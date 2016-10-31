9e8a50fa5d5cd622b0e2a7ef20f7916404601824
jack775544 <jack775544@gmail.com>
2016-10-31 11:53:11 +1000

    Added solve step
    Added solve step that will generate the histogram data and cache the
    result in the session data set.

--- color_histogram/histogram.py
+++ color_histogram/histogram.py
@@ -12,18 +12,19 @@ or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
 """
 
 from tropofy.app import AppWithDataSets, Step, StepGroup, ParameterGroup, Parameter
-from tropofy.widgets import ParameterForm, Chart
+from tropofy.widgets import ParameterForm, Chart, ExecuteFunction
 import requests
 from PIL import Image
 from StringIO import StringIO
 from collections import OrderedDict
+from tropofy.widgets.text_widget import TextWidget
 
 MAX_NUM = int('FFFFFF', 16)
-PARTITIONS = 35
+PARTITIONS = 65
 
 
 class UrlParameterGroup(ParameterGroup):
-    URL = Parameter(name='url', label='Image Url', default='https://tropofy.com/static/css/img/get_started.png', allowed_type=str)
+    URL = Parameter(name='url', label='Image Url', default='http://j754.xyz/other/image.jpg', allowed_type=str)
 
 
 class ColorHistogram(Chart):
@@ -31,32 +32,7 @@ class ColorHistogram(Chart):
         return Chart.BARCHART
 
     def get_table_data(self, app_session):
-        # Get the image
-        link = app_session.data_set.get_param(UrlParameterGroup.URL.name)
-        response = requests.get(link)
-        img = Image.open(StringIO(response.content))
-
-        # Get RGB values for the image
-        rgb_img = img.convert('RGBA')
-        width, height = rgb_img.size
-
-        # Create buckets for each color to go into
-        keys = []
-        for i in xrange(PARTITIONS + 1):
-            keys.append((MAX_NUM / PARTITIONS) * i)
-        result_dict = OrderedDict(sorted({key: 0 for key in keys}.items()))
-
-        # For each pixel get the base 16 representation of the color and place it into the closest bucket
-        for x in xrange(width):
-            for y in xrange(height):
-                r, b, g, a = rgb_img.getpixel((x, y))
-                hex_code = int(str(r) + str(g) + str(b), 16)
-                result_dict[min(result_dict, key=lambda p:abs(p - hex_code))] += 1
-
-        # The total number of pixels is width * height
-        # Use this value to normalise the values in the map
-        total = width * height
-        return [{"color": '#' + hex(k)[2:].upper(), "pct": float(v)/total} for k, v in result_dict.iteritems()]
+        return app_session.data_set.get_var('data')
 
     def get_table_schema(self, app_session):
         return {
@@ -71,6 +47,42 @@ class ColorHistogram(Chart):
         return "color"
 
 
+class CreateHistogram(ExecuteFunction):
+    def get_button_text(self, app_session):
+        return "Create Histogram"
+
+    def execute_function(self, app_session):
+        link = app_session.data_set.get_param(UrlParameterGroup.URL.name)
+        response = requests.get(link)
+        img = Image.open(StringIO(response.content))
+
+        # Get RGB values for the image
+        rgb_img = img.convert('RGBA')
+        width, height = rgb_img.size
+        total = width * height
+        
+        app_session.task_manager.send_progress_message("Image Width: " + str(width) + "px")
+        app_session.task_manager.send_progress_message("Image Height: " + str(height) + "px")
+        app_session.task_manager.send_progress_message("Total Image Size: " + str(total) + "px")
+        # Create buckets for each color to go into
+        keys = []
+        for i in xrange(PARTITIONS + 1):
+            keys.append((MAX_NUM / PARTITIONS) * i)
+        result_dict = OrderedDict(sorted({key: 0 for key in keys}.items()))
+
+        # For each pixel get the base 16 representation of the color and place it into the closest bucket
+        for x in xrange(width):
+            for y in xrange(height):
+                r, b, g, a = rgb_img.getpixel((x, y))
+                hex_code = int(str(r) + str(g) + str(b), 16)
+                result_dict[min(result_dict, key=lambda p:abs(p - hex_code))] += 1
+
+        # Normalise the values in the map
+        app_session.data_set.set_var('data', [{"color": '#' + hex(k)[2:].upper(), "pct": float(v)/total} for k, v in result_dict.iteritems()])
+        app_session.task_manager.send_progress_message("Histogram successfully created. Go to next step to view it.")
+
+
+
 class HistogramApp(AppWithDataSets):
     def get_name(self):
         return "Color Histogram App"
@@ -80,6 +92,10 @@ class HistogramApp(AppWithDataSets):
             StepGroup(name='Input', steps=[
                 Step(name='URL', widgets=[
                     ParameterForm()
+                ]),
+                Step(name='Generate', widgets=[
+                    TextWidget(text='Generate the histogram, this may take a little while'),
+                    CreateHistogram()
                 ])
             ]),
             StepGroup(name='Output', steps=[
@@ -91,3 +107,12 @@ class HistogramApp(AppWithDataSets):
 
     def get_parameters(self):
         return UrlParameterGroup.get_params()
+    
+    def get_home_page_content(self):
+        return {
+            'content_app_name_header': '<div style="align: center;">Color Histogram</div>',
+            'content_single_column_app_description': 
+                """
+                <p>An app that can generate a colour histogram from an image hosted on the internet</p>
+                """
+        }
